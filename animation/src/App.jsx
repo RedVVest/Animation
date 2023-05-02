07aee4d6a7f9203784ba2cba8433cd1537ffa06c
RedVVest <nadolskyy.fake@gmail.com>
2023-05-02 20:15:52 +0300

    second animation done

--- animation/src/App.jsx
+++ animation/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import SectionOne from "./Components/SectionOne.jsx";
+import SectionTwo from "./Components/SectionTwo.jsx";
 
 function App() {
   return (
     <>
         <SectionOne/>
+        <SectionTwo/>
     </>
   )
 }
